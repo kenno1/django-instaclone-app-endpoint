d8e94d1ca9262c2d00907d68887535e6d3645e88
kambe.kenta <kenta.k199405@gmail.com>
2021-01-15 21:31:24 +0900

    added comment class.

--- api/models.py
+++ api/models.py
@@ -66,4 +66,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    text = models.CharField(max_length=100)
+    userComment = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='userComment',
+        on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
 
