b3f41dd05427c3cbe82e60a789488e871ddab3a5
kambe.kenta <kenta.k199405@gmail.com>
2021-01-28 19:44:16 +0900

    created serializers and added ProfileSerializer.

--- api/serializers.py
+++ api/serializers.py
@@ -13,3 +13,10 @@ class UserSerializer(serializers.ModelSerializer):
         user = get_user_model().objects.create_user(**validated_data)
         return user
 
+class ProfileSerializer(serializers.ModelSerializer):
+    created_on = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+    class Meta:
+        model=Profile
+        fields = ('id', 'nickName', 'userProfile', 'created_on', 'img')
+        extra_kwargs = {'userProfile': {'read_only': True}}
+
