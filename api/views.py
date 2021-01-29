315d84bc201446771cbc7711ead507772ba940a2
kambe.kenta <kenta.k199405@gmail.com>
2021-01-30 08:47:09 +0900

    added create user view.

--- api/views.py
+++ api/views.py
@@ -4,3 +4,7 @@ from rest_framework.permissions import AllowAny
 from . import serializers
 from .models import Profile, Post, Comment
 
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
+
