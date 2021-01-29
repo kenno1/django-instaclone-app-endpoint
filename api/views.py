b7a18e636dbebc113adb5a278bc263d2b5a6d588
kambe.kenta <kenta.k199405@gmail.com>
2021-01-30 08:45:19 +0900

    added modules.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from . import serializers
+from .models import Profile, Post, Comment
 
-# Create your views here.
